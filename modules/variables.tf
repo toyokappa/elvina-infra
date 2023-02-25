1e4b91b69557901c4362e8dda3023489620538e6
Yuta Toyokawa <kppg42@gmail.com>
2023-02-26 02:13:15 +0900

    LP用環境の構築

---
+++ modules/variables.tf
@@ -0,0 +1,7 @@
+locals {
+  app_name           = "elvina"
+  aws_account_id     = "648401562011"
+  lp_domain          = "elvina.jp"
+  host_zone_id       = "Z045038139NH8JXJD6E55"
+  cf_certificate_arn = "arn:aws:acm:us-east-1:648401562011:certificate/fbe7ad02-4aa0-4174-9da4-05a19fa0acb0"
+}
